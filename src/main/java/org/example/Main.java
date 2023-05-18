ece860f87402b4c803d2a6c832b382d48343985a
biao <biao@cao.com>
2023-05-18 20:55:12 +0800

    忘记符号; commit idea

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("hot-fix-1");
         System.out.println("hot-fix-2");
         System.out.println("push test");
-        System.out.println("pull test")
+        System.out.println("pull test");
         // 按 Shift+F10 或点击间距中的绿色箭头按钮以运行脚本。
         for (int i = 1; i <= 5; i++) {
 
